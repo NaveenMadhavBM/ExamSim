a657c59d989dde9c7f444352df16592e491e86ee
NaveenMadhavBM <Naveen.madhav.bm@gmail.com>
2021-01-13 12:39:03 +0530

    TESTING DONE, ready to delpy version 1.0

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Course)
+admin.site.register(Taken)
+admin.site.register(Module)
+admin.site.register(Result)
+admin.site.register(Question)
+admin.site.register(Answer_Sheet)
